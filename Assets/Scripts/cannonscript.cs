94185912c9cb1522d0b80260ee132fd854c55d48
ryayan855 <theokayfan@gmail.com>
2024-10-18 14:55:51 -0700

    bullet/canon colliders and hitboxes added

---
+++ Assets/Scripts/cannonscript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonscript : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject bullet;
+    float timeBetween;
+    public float startTimeBetween;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBetween = startTimeBetween;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeBetween <=0){
+            Instantiate(bullet, firepoint.position, firepoint.rotation);
+            timeBetween = startTimeBetween;
+        }
+        else{
+            timeBetween -= Time.deltaTime;
+        }
+    }
+}
