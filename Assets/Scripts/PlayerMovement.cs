633e4ca5615471a0b21ecfbf3b7a1eb9811c0021
Jammmmmmm <jamesdavidwang@gmail.com>
2024-10-17 19:38:05 -0700

    Directional Attack Animations and Player States

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,30 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState{
+    walk,
+    attack,
+    interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D myRigidBody;
     private Vector3 change;
     private Animator anim;
 
+    
+
     // Start is called before the first frame update
     void Start() {
+        currentState = PlayerState.walk;
         anim = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void FixedUpdate() {
+    void Update() {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMovement();
+        
+        if(Input.GetButtonDown("Attack") && currentState != PlayerState.attack){
+            StartCoroutine(AttackCo());
+        }
+
+        else if(currentState == PlayerState.walk){
+            UpdateAnimation();
+        }
     }
 
-    void UpdateAnimationAndMovement(){
-        if(change != Vector3.zero){
+    void FixedUpdate() {
+        // Handle movement in FixedUpdate for smooth physics-based movement
+        if (change != Vector3.zero && currentState == PlayerState.walk) {
             MoveCharacter();
+        }
+    }
+
+    private IEnumerator AttackCo(){
+        anim.SetBool("Attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        anim.SetBool("Attacking", false);
+        yield return new WaitForSeconds(.33f);
+        currentState = PlayerState.walk;
+    }
+
+    void UpdateAnimation(){
+        if(change != Vector3.zero){
             anim.SetFloat("MoveX", change.x);
             anim.SetFloat("MoveY", change.y);
             anim.SetBool("Moving", true);
